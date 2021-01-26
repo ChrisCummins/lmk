a844cac56cccd5bbb92eb22363aa1738b7fc8fc3
Chris Cummins <cummins@fb.com>
2021-01-26 07:02:58 +0000

    Add long description file.

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup
 
-with open('./requirements.txt') as infile:
+with open("./requirements.txt") as infile:
     requirements = [x.strip() for x in infile.readlines() if x.strip()]
 
-setup(name='lmk',
-      version='0.0.15',
-      description='Email notification when command completes',
-      url='https://github.com/ChrisCummins/lmk',
-      author='Chris Cummins',
-      author_email='chrisc.101@gmail.com',
-      license='MIT License',
-      scripts=['lmk'],
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+      author="Chris Cummins",
+      author_email="chrisc.101@gmail.com",
+      description="Email notification when command completes",
       install_requires=requirements,
-      zip_safe=True)
+      license="MIT License",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      name="lmk",
+      scripts=["lmk"],
+      url="https://github.com/ChrisCummins/lmk",
+      version="0.0.16",
+      zip_safe=True,
+)
